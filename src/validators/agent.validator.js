ca7ec9350401dda1f3ae0b791394dc1085d0b4ae
03karthikeyan <karthikleeromeo21@gmail.com>
2026-08-19 16:24:05 +0530

    Update backend changes

--- src/validators/agent.validator.js
+++ src/validators/agent.validator.js
@@ -7,10 +7,25 @@ const createAgentSchema = {
     password: Joi.string().min(6).required(),
     phone: Joi.string().required(),
     agentCode: Joi.string().alphanum().uppercase().optional(),
-    branchId: Joi.string().allow(null).optional(),
+    branchId: Joi.string().allow(null, '').optional(),
     assignedRoutes: Joi.array().items(Joi.string()).optional(),
     dailyTarget: Joi.number().min(0).optional(),
     commissionPercentage: Joi.number().min(0).max(100).optional(),
+    profileImage: Joi.string().allow('', null).optional(),
+    proofType: Joi.string().allow('', null).optional(),
+    proofNumber: Joi.string().allow('', null).optional(),
+    emergencyContact: Joi.object({
+      name: Joi.string().allow('').optional(),
+      phone: Joi.string().allow('').optional(),
+      relation: Joi.string().allow('').optional(),
+    }).optional(),
+    address: Joi.object({
+      street: Joi.string().allow('').optional(),
+      city: Joi.string().allow('').optional(),
+      district: Joi.string().allow('').optional(),
+      state: Joi.string().allow('').optional(),
+      pincode: Joi.string().allow('').optional(),
+    }).optional(),
   }),
 };
 
@@ -18,11 +33,27 @@ const updateAgentSchema = {
   body: Joi.object({
     name: Joi.string().optional(),
     phone: Joi.string().optional(),
-    branchId: Joi.string().allow(null).optional(),
+    branchId: Joi.string().allow(null, '').optional(),
     assignedRoutes: Joi.array().items(Joi.string()).optional(),
     dailyTarget: Joi.number().min(0).optional(),
     commissionPercentage: Joi.number().min(0).max(100).optional(),
     status: Joi.string().valid('ACTIVE', 'INACTIVE', 'SUSPENDED').optional(),
+    password: Joi.string().min(6).allow('', null).optional(),
+    profileImage: Joi.string().allow('', null).optional(),
+    proofType: Joi.string().allow('', null).optional(),
+    proofNumber: Joi.string().allow('', null).optional(),
+    emergencyContact: Joi.object({
+      name: Joi.string().allow('').optional(),
+      phone: Joi.string().allow('').optional(),
+      relation: Joi.string().allow('').optional(),
+    }).optional(),
+    address: Joi.object({
+      street: Joi.string().allow('').optional(),
+      city: Joi.string().allow('').optional(),
+      district: Joi.string().allow('').optional(),
+      state: Joi.string().allow('').optional(),
+      pincode: Joi.string().allow('').optional(),
+    }).optional(),
   }),
 };
 
